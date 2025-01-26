93fe0092b1a5ce6c8500717ea71dc46ab6d76b11
Aman <aman13032006@gmail.com>
2025-01-26 09:08:38 +0600

    Прописал пути эндпоинтов в urls.py

--- app/views.py
+++ app/views.py
@@ -4,7 +4,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from django.contrib.auth.models import User
 
 from .models import Patient
 from .serializers import PatientSerializer
@@ -21,11 +20,13 @@ class LoginView(APIView):
         if not username or not password:
             return Response({'username': 'This field is required.', 'password': 'This field is required.'},
                             status=status.HTTP_400_BAD_REQUEST)
-
+        # Проверяем учетные данные пользователя
         user = authenticate(username=username, password=password)
         if user is not None:
             refresh = RefreshToken.for_user(user)
+            # Refresh-токен для обновления access-токена
             access_token = str(refresh.access_token)
+            # Access-токен для авторизации
             return Response({"access": access_token, "refresh": str(refresh)}, status=status.HTTP_200_OK)
         else:
             return Response({"detail": "Invalid username or password"}, status=status.HTTP_401_UNAUTHORIZED)
@@ -43,3 +44,4 @@ class PatientListView(APIView):
         patients = Patient.objects.all()
         serializer = PatientSerializer(patients, many=True)
         return Response(serializer.data)
+
