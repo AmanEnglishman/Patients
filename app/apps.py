7c0140f825c1a8bc7cbc4b2233ed9617f989db36
Aman <aman13032006@gmail.com>
2025-04-01 16:35:29 +0600

    commit

--- app/apps.py
+++ app/apps.py
@@ -5,11 +5,8 @@ class AppPatientConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
 
-    # Создаем роль doctor, если его нет при запуске проекта
     def ready(self):
-        from django.contrib.auth.models import Group
-        Group.objects.get_or_create(name='doctor')
-
+        import app.signals
 
 
 
